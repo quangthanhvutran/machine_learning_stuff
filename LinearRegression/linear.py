78de360e2694756c3cced72270c9391e8186ad4f
quangthanhvutran <letsbeabstinent@gmail.com>
2023-09-18 19:59:31 +0700

    lmaolmao

--- LinearRegression/linear.py
+++ LinearRegression/linear.py
@@ -1,5 +1,7 @@
 import numpy as np
 
-class HomeMadeRegression:
+class HomeMadeLinearRegression:
+    def load_data(self, x, y):
+        self.data = x
+        self.data = y
     def gradient_descent(w_in, b_in, cost_function, compute_gradient, alpha, num_iters):
-        
